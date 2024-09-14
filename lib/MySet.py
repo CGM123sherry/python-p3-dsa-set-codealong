845954f038669071f944113c61376c46b47d91ec
CGM123sherry <g.christinemurithi@gmail.com>
2024-09-14 14:21:39 +0300

    completed the dsa set lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable=[]):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def __str__(self):
+        set_list = sorted(self.dictionary.keys())  # Sort to match output in the test
+        return f'MySet: {{{",".join(map(str, set_list))}}}'  # Converts list to string format
+
+    def add(self, value):
+        self.dictionary[value] = True  # Ensures no duplicates, just adds the key
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)  # Removes the value if present, else does nothing
+        return self
+
+    def size(self):
+        return len(self.dictionary)  # Returns the size of the set (number of keys)
+
+    def clear(self):
+        self.dictionary.clear()  # Clears all elements from the set
+        return self
